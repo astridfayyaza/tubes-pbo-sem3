47f7a412a5a5ec50cef559ba8ac5e92e53b38bae
Dakai-kun <nezgaming94@gmail.com>
2025-12-18 14:45:52 +0700

    little change

--- detail_transaksi.java
+++ detail_transaksi.java
@@ -1,5 +1,8 @@
 public class detail_transaksi {
     private int detail_id;
+    private int transaksi_id;
+    private int product_id;
+    private int user_id;
     private String nama_barang;
     private float harga;
     private int jumlah;
